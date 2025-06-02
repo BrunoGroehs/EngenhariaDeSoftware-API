c3d7de682b609807bec58947af6f13c53514788b
BrunoGroehs <brunogroehs@gmail.com>
2025-06-02 18:32:31 -0300

    fix

---
+++ test/setup.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const sinon = require('sinon');
+
+// Make expect globally available
+global.expect = chai.expect;
+
+// Set up test environment
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'test-secret-key';
+process.env.DB_HOST = 'localhost';
+process.env.DB_PORT = '5432';
+process.env.DB_NAME = 'test_db';
+process.env.DB_USER = 'test_user';
+process.env.DB_PASSWORD = 'test_password';
+
+// Global sinon sandbox for easier cleanup
+global.sinon = sinon;
+
+// Set up hooks for test cleanup
+beforeEach(function() {
+  // Create a new sandbox for each test
+  this.sandbox = sinon.createSandbox();
+});
+
+afterEach(function() {
+  // Restore all stubs after each test
+  if (this.sandbox) {
+    this.sandbox.restore();
+  }
+  sinon.restore();
+});
